32595459014e27f45d74318f63aa2858b2e85422
JoshGibbons <jgibbons093@gmail.com>
2015-04-29 22:18:28 -0400

    gui started

--- main.py
+++ main.py
@@ -5,12 +5,14 @@
 '''
 
 import twitter
-import tweepy  # twitter api module - python version
+import Tkinter
+import tweepy
 import datetime  # python datetime module
 import imp
 import sys
-import json  # python json module
+import json
 import authKeys
+from PIL import Image, ImageTk
 
 #utf check
 imp.reload(sys)
@@ -31,11 +33,24 @@ auth.set_access_token(access_token_key, access_token_secret)
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
 
+#create gui
+window = Tkinter.Tk()
+window.geometry("1296x810+50+10")
+window.title("Project name here");
+
+#background
+im = Image.open('bckg.jpg')
+tkimage = ImageTk.PhotoImage(im)
+myvar=Tkinter.Label(window,image = tkimage)
+myvar.place(x=0, y=0, relwidth=1, relheight=1)
+
+window.mainloop()
+
 
 def main():
-
-    for user in tweepy.Cursor(api.followers, screen_name="GreatPowerKyle").items():
-        print user.screen_name
+    print("hello")
+    #for user in tweepy.Cursor(api.followers, screen_name="GreatPowerKyle").items():
+    #    print user.screen_name
 
 if __name__ == '__main__':
     main()
