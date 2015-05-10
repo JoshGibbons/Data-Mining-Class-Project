a31d2916b554062e9d01ed7fc6565fd80bbffea2
David Noftsier <dnoftsier@gmail.com>
2015-05-10 09:06:48 -0400

    Update main.py for portability

--- main.py
+++ main.py
@@ -13,13 +13,12 @@ import authKeys
 import basic_and_general_info
 import write_and_read_objects
 import users_retweeted_most
-# utf check
 import interface
 import sent
 import basicInfo
 imp.reload(sys)
 sys.setdefaultencoding("utf-8")
-
+# utf check
 
 # Private keys
 consumer_key = authKeys.getA()
@@ -34,13 +33,12 @@ auth.set_access_token(access_token_key, access_token_secret)
 # Creation of the api, using authentication
 api = tweepy.API(auth)
 
-<<<<<<< HEAD
 username = 'gavinfree'
 
 def main():
  '''
  interface.createGUI()
-
+ 
  def getUserData(username, window):
  print(username)
 
@@ -54,7 +52,7 @@ def main():
  '''
 
  # This generates the 'tweet_objects.txt' and returns tweet_object_list
- tweet_object_list = write_and_read_objects.writeTweetObjectsToFile(api, username)
+ #tweet_object_list = write_and_read_objects.writeTweetObjectsToFile(api, username)
 
  # Only used for testing (when you have previously generated 'tweet_objects.txt' don't exceed your access limit)
  #tweet_object_list = write_and_read_objects.readJsonFile()
@@ -63,10 +61,10 @@ def main():
   #print tweet["text"]
   #pass
  
- basic_and_general_info.showBasicInfo(tweet_object_list)
- basic_and_general_info.showGeneralInfo(tweet_object_list) 
+ #basic_and_general_info.showBasicInfo(tweet_object_list)
+ #basic_and_general_info.showGeneralInfo(tweet_object_list) 
 
- users_retweeted_most.showUsersRetweetedMost(tweet_object_list)
+ #users_retweeted_most.showUsersRetweetedMost(tweet_object_list)
 
  #basicInfo.showBasicInfo(username)
  #sent.getSentiment(tweets, window)
