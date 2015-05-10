59ebb99b47cb26b027bd94315022956cb3230082
David Noftsier <dnoftsier@gmail.com>
2015-05-10 11:10:59 -0400

    Final Merges complete for main.py and interface.py

--- main.py
+++ main.py
@@ -41,17 +41,31 @@ username = 'gavinfree'
 
 def main():
  
- interface.createGUI()
- 
- # def getUserData(username, window):
- # print(username)
-
  # This generates the 'tweet_objects.txt' and returns tweet_object_list
  #tweet_object_list = write_and_read_objects.writeTweetObjectsToFile(api, username)
 
  # Only used for testing (when you have previously generated 'tweet_objects.txt' don't exceed your access limit)
  #tweet_object_list = write_and_read_objects.readJsonFile()
+
+ #interface.createGUI()
  
+ #def getUserData(username, window):
+  #print(username)
+  # Storing a list of statuses
+  '''
+  tweetStatuses = []
+  count = 0
+  for tweet in tweet_object_list:
+   tweetStatuses.append(tweet['text'])
+   count += 1
+   if count > 99:
+    break
+  '''
+ 
+ #sent.getSentiment(tweetStatuses, window)
+
+ 
+ # Example for getting the statuses of tweet objects
  #for tweet in tweet_object_list:
   #print tweet["text"]
   #pass
@@ -60,20 +74,6 @@ def main():
  #basic_and_general_info.showGeneralInfo(tweet_object_list) 
 
  #users_retweeted_most.showUsersRetweetedMost(tweet_object_list)
-
- # Storing a list of statuses
-
- '''
- tweetStatuses = []
- count = 0
- for tweet in tweet_object_list:
-  tweetStatuses.append(tweet['text'])
-  count += 1
-  if count > 99:
-   break
- '''
-
- #sent.getSentiment(tweetStatuses, window)
 
  #for user in tweepy.Cursor(api.followers, screen_name = "GreatPowerKyle").items(): #print user.screen_name
 
