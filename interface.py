9700ae223f188474dfea2ca61e28ad2cc6ee4db0
David Noftsier <dnoftsier@gmail.com>
2015-05-10 08:25:17 -0400

    Interface

---
+++ interface.py
@@ -0,0 +1,66 @@
+import Tkinter 
+from PIL import Image, ImageTk
+import main
+
+# Globals
+img = None
+
+def createGUI(): #create window
+	window = Tkinter.Tk()
+	window.geometry("1296x810+50+10")
+	window.title("Twitter Profiler");
+
+#
+background
+global img
+load = Image.open('bckg.jpg')
+render = ImageTk.PhotoImage(load)
+img = Tkinter.Label(window, image = render)
+img.image = render
+img.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+
+# textbox
+v = Tkinter.StringVar()
+txtBox = Tkinter.Entry(window, textvariable = v, width = 33, relief = 'flat')
+txtBox.pack()
+txtBox.place(x = 840, y = 254)
+txtBox.focus_set()# event listener
+txtBox.bind('<Return>', lambda event: handleEvent(v, window, txtBox))
+
+# run
+window.mainloop()
+
+def handleEvent(v, window, txtBox): #grab username
+	username = v.get()# destroy textbox
+	txtBox.pack_forget()
+	txtBox.unbind('<Return>')# change background
+	global img
+	img.destroy()
+	load = Image.open('back2.png')
+	render = ImageTk.PhotoImage(load)
+	img = Tkinter.Label(window, image = render)
+	img.image = render
+	img.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+
+displayName(username, window)
+main.getUserData(username, window)
+
+def displayName(username, window):
+    name = Tkinter.Label(window, text = username, font = ("arial", 28), bg = "#1a4d84", fg = 'white')
+    name.place(x = 60, y = 40)
+
+def showDataWarning(window):
+    warning = Tkinter.Label(window,
+        text = "*User may not have enough tweets for an accurate profile",
+        font = ("arail", 18),
+        bg = "#1a4d84",
+        fg = 'white')
+	warning.place(x = 60, y = 60)
+
+def displaySentScore(window, score):
+    warning = Tkinter.Label(window,
+        text = score,
+        font = ("arail", 22),
+        bg = "#1a4d84",
+        fg = 'white')
+	warning.place(x = 60, y = 80)
